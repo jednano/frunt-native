628c4df93631c94bbdce661a3d8fdf1632f86e7e
Jed Mao <jedmao@outlook.com>
2018-04-24 09:34:18 -0500

    Move App to src

--- App.tsx
+++ App.tsx
@@ -1,9 +1,1 @@
-import * as React from 'react';
-
-import RootStack from './src/screens';
-
-export default class App extends React.Component {
-	render() {
-		return <RootStack />;
-	}
-}
+export { default } from './src/App';
